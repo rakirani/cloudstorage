3f9157a7cc9a2f699789dd25ee379ee84c962634
rajani <rakirani.learning@gmail.com>
2021-03-29 01:03:22 -0400

    .

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/FilesMapper.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/FilesMapper.java
@@ -13,8 +13,8 @@ public interface FilesMapper {
     @Select("Select * from Files where fileid=#{Fileid} ")
     Files getFile(int Fileid);
 
-    @Insert("Insert into Files(filename,contenttype,filesize,filedata) values(#{filename},#{contenttype},#{filesize}" +
-            ",#{filedata})")
+    @Insert("Insert into Files(filename,contenttype,filesize,userid,filedata) values(#{filename},#{contenttype},#{filesize}" +
+            ",#{userid},#{filedata})")
     @Options(useGeneratedKeys = true,keyProperty = "fileid")
     int insertFile(Files files);
 
